d23a1ed1aca8a22521fbee4d6fe8d37749238aef
Eduardo Bombo <eduardo.bombo@outlook.com>
2022-10-25 15:56:11 -0300

    if e switch

---
+++ controle-de-fluxo/src/decisaoIf.java
@@ -0,0 +1,34 @@
+public class decisaoIf {
+    public static void main(String[] args) {
+
+        int mes = 6;
+        
+        if (mes == 1) {
+            System.out.print("Janeiro");
+        } else if (mes == 2) {
+            System.out.print("Fevereiro");
+        } else if (mes == 3) {
+            System.out.print("Março");
+        } else if (mes == 4) {
+            System.out.print("Abril");
+        } else if (mes == 5) {
+            System.out.print("Maio");
+        } else if (mes == 6) {
+            System.out.print("Junho");
+        } else if (mes == 7) {
+            System.out.print("Julho");
+        } else if (mes == 8) {
+            System.out.print("Agosto");
+        } else if (mes == 9) {
+            System.out.print("Setembro");
+        } else if (mes == 10) {
+            System.out.print("Outubro");
+        } else if (mes == 11) {
+            System.out.print("Novembro");
+        } else if (mes == 12) {
+            System.out.print("Dezembro");
+        } else {
+            System.out.print("Mês inválido");
+        }
+    }
+}
