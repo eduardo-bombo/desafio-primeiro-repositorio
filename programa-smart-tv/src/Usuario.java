c8f5081f4346686d296c530b785666662d4c29a5
Eduardo Bombo <eduardo.bombo@outlook.com>
2022-10-24 21:47:46 -0300

    Mais interações

--- programa-smart-tv/src/Usuario.java
+++ programa-smart-tv/src/Usuario.java
@@ -7,7 +7,12 @@ public class Usuario {
         smartTv.aumentarVolume();
         smartTv.diminuirVolume();
         smartTv.aumentarVolume();
-        System.out.println("Volume atual: " + smartTv.volume); 
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        System.out.println("Canal atual: " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal atual: " + smartTv.canal);
+
 
         System.out.println("TV ligada? " + smartTv.ligada);
         System.out.println("Canal atual: " + smartTv.canal);
